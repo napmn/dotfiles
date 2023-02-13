8a1cb892e628976570bd0c3e7183d1b3e462e6c5
napmn <lukass135@gmail.com>
2023-02-13 17:28:45 +0100

    Updated cmp setting structure

--- nvim/lua/plug-config/cmp.lua
+++ nvim/lua/plug-config/cmp.lua
@@ -108,8 +108,10 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  window = {
+    documentation = {
+      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    }
   },
   experimental = {
     ghost_text = false,
